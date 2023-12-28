6cb9e82ed9916cdc621835f77d653a3f6803445c
yair <yairsimantov20@gmail.com>
2023-12-28 16:45:06 +0200

    logs

--- pkg/defaults/init.go
+++ pkg/defaults/init.go
@@ -46,7 +46,7 @@ func InitIntegration(portClient *cli.PortClient, applicationConfig *port.Config)
 		// Handle a deprecated case where resources are provided in config file
 		return integration.CreateIntegration(portClient, applicationConfig.StateKey, applicationConfig.EventListenerType, defaultIntegrationConfig)
 	} else {
-		klog.Infof("Integration exists, patching it")
+		klog.Infof("Integration with state key %s already exists, patching it", applicationConfig.StateKey)
 		integrationPatch := &port.Integration{
 			EventListener: getEventListenerConfig(applicationConfig.EventListenerType),
 		}
