8e03ee0988959531a894d91540cd479f99f5b30c
talsabagport <tal@getport.io>
2024-08-14 18:48:18 +0300

    fix tests

--- pkg/k8s/controller_test.go
+++ pkg/k8s/controller_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/port-labs/port-k8s-exporter/pkg/jq"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/port-labs/port-k8s-exporter/pkg/config"
@@ -265,45 +264,6 @@ func TestCreateDeployment(t *testing.T) {
 
 	f := newFixture(t, &fixtureConfig{resource: resource, objects: objects})
 	f.runControllerSyncHandler(item, &SyncResult{EntitiesSet: map[string]interface{}{fmt.Sprintf("%s;%s", blueprint, d.Name): nil}, RawDataExamples: []interface{}{ud.Object}, ShouldDeleteStaleEntities: true}, false)
-}
-
-func TestJqSearchRelation(t *testing.T) {
-
-	mapping := []port.EntityMapping{
-		{
-			Identifier: ".metadata.name",
-			Blueprint:  fmt.Sprintf("\"%s\"", blueprint),
-			Icon:       "\"Microservice\"",
-			Team:       "\"Test\"",
-			Properties: map[string]string{},
-			Relations: map[string]interface{}{
-				"k8s-relation": map[string]interface{}{
-					"combinator": "\"or\"",
-					"rules": []interface{}{
-						map[string]interface{}{
-							"property": "\"$identifier\"",
-							"operator": "\"=\"",
-							"value":    "\"e_AgPMYvq1tAs8TuqM\"",
-						},
-					},
-				},
-			},
-		},
-	}
-	res, _ := jq.ParseMapRecursively(mapping[0].Relations, nil)
-	assert.Equal(t, res, map[string]interface{}{
-		"k8s-relation": map[string]interface{}{
-			"combinator": "or",
-			"rules": []interface{}{
-				map[string]interface{}{
-					"property": "$identifier",
-					"operator": "=",
-					"value":    "e_AgPMYvq1tAs8TuqM",
-				},
-			},
-		},
-	})
-
 }
 
 func TestCreateDeploymentWithSearchRelation(t *testing.T) {
