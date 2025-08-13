aea4432f9c415aba198f8b504fd08317ed233060
omribarouch <omrib@getport.io>
2025-08-13 09:19:54 +0300

    fix test

--- pkg/handlers/controllers_test.go
+++ pkg/handlers/controllers_test.go
@@ -603,3 +603,22 @@ func TestControllersHandler_Stop(t *testing.T) {
 	assert.True(t, f.controllersHandler.isStopped)
 	assert.Panics(t, func() { close(f.controllersHandler.stopCh) })
 }
+
+func TestControllersHandler_RunResyncNotOverlaps(t *testing.T) {
+	stateKey := guuid.NewString()
+	resources := []port.Resource{getBaseResource(stateKey, deploymentKind)}
+	f := newFixture(t, &fixtureConfig{stateKey: stateKey, resources: resources, existingObjects: []runtime.Object{}})
+	defer tearDownFixture(t, f)
+
+	RunResync(&port.Config{StateKey: stateKey}, f.k8sClient, f.portClient, SCHEDULED_RESYNC)
+	firstControllersHandler := controllerHandler
+
+	RunResync(&port.Config{StateKey: stateKey}, f.k8sClient, f.portClient, SCHEDULED_RESYNC)
+	secondControllersHandler := controllerHandler
+
+	assert.NotNil(t, firstControllersHandler)
+	assert.NotNil(t, secondControllersHandler)
+	assert.NotEqual(t, firstControllersHandler, secondControllersHandler)
+	assert.True(t, firstControllersHandler.isStopped)
+	assert.False(t, secondControllersHandler.isStopped)
+}
