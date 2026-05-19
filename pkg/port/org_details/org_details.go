2dd94a3c14977af232257478cf794c6b464039fb
Hadar-Vaknin <hadarvaknin1231@gmail.com>
2026-05-19 14:45:27 +0300

    fix

--- pkg/port/org_details/org_details.go
+++ pkg/port/org_details/org_details.go
@@ -26,7 +26,6 @@ func GetOrganizationFeatureFlags(portClient *cli.PortClient) ([]string, error) {
 }
 
 func ShouldUseIntegrationResyncRequestsTopic(portClient *cli.PortClient) (bool, error) {
-	return false, nil
 	flags, err := GetOrganizationFeatureFlags(portClient)
 	if err != nil {
 		return false, err
